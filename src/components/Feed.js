d7dec40f6de7d3d0c882a5eecd83a93e581e5c30
guilherme2020 <guilhermerodrigues123477@gmail.com>
2019-03-02 15:53:42 -0300

    integrando login e configuracao da biblioteca de rotas,comunicacao com api

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -52,62 +52,111 @@ export default class Feed extends Component {
           })
       );
   }
-  like(idFoto) {
-    const foto = this.state.fotos.find(foto => foto.id === idFoto);
 
-    let novaLista = [];
-
-    if (!foto.likeada) {
-      novaLista = [...foto.likers, { login: "meuUsuario" }];
-    } else {
-      novaLista = foto.likers.filter(liker => {
-        return liker.login != "meuUsuario";
-      });
-    }
-
-    const fotoAtualizada = {
-      ...foto,
-      likeada: !foto.likeada,
-      likers: novaLista
-    };
-
+  buscaPorId(idFoto) {
+    return this.state.fotos.find(foto => foto.id === idFoto);
+  }
+  atualizaFotos(fotoAtualizada) {
     const fotos = this.state.fotos.map(foto =>
       foto.id === fotoAtualizada.id ? fotoAtualizada : foto
     );
+    this.setState({ fotos });
+  }
+  like(idFoto) {
+    // const foto = this.state.fotos.find(foto => foto.id === idFoto);
+    const foto = this.buscaPorId(idFoto);
+    AsyncStorage.getItem("usuario")
+      .then(usuarioLogado => {
+        let novaLista = [];
 
-    this.setState({
-      fotos
-    });
+        if (!foto.likeada) {
+          novaLista = [...foto.likers, { login: usuarioLogado }];
+        } else {
+          novaLista = foto.likers.filter(liker => {
+            return liker.login != usuarioLogado;
+          });
+        }
+        return novaLista;
+      })
+      .then(novaLista => {
+        const fotoAtualizada = {
+          ...foto,
+          likeada: !foto.likeada,
+          likers: novaLista
+        };
+
+        // const fotos = this.state.fotos.map(foto =>
+        //   foto.id === fotoAtualizada.id ? fotoAtualizada : foto
+        // );
+
+        // this.setState({
+        //   fotos
+        // });
+        this.atualizaFotos(fotoAtualizada);
+      });
+    let url = `https://instalura-api.herokuapp.com/api/fotos/${idFoto}/like`;
+
+    AsyncStorage.getItem("token")
+      .then(token => {
+        return {
+          method: "POST",
+          headers: new Headers({
+            "X-AUTH-TOKEN": token
+          })
+        };
+      })
+      .then(requestInfo => fetch(url, requestInfo));
   }
   adicionaComentario(idFoto, valorComentario, inputComentario) {
     if (valorComentario === "") {
       return;
     }
 
-    const foto = this.state.fotos.find(foto => foto.id === idFoto);
+    // const foto = this.state.fotos.find(foto => foto.id === idFoto);
+    const foto = this.buscaPorId(idFoto);
+    const uri = `https://instalura-api.herokuapp.com/api/fotos/${idFoto}/comment`;
+    AsyncStorage.getItem("token")
+      .then(token => {
+        return {
+          method: "POST",
+          body: JSON.stringify({
+            texto: valorComentario
+          }),
+          headers: new Headers({
+            "Content-type": "application/json",
+            "X-AUTH-TOKEN": token
+          })
+        };
+      })
+      .then(requestInfo => fetch(uri, requestInfo))
+      .then(resposta => resposta.json())
+      .then(comentario => [...foto.comentarios, comentario])
+      .then(novaLista => {
+        const fotoAtualizada = {
+          ...foto,
+          comentarios: novaLista
+        };
+        this.atualizaFotos(fotoAtualizada);
+        inputComentario.clear();
+      });
 
-    const novaLista = [
-      ...foto.comentarios,
-      {
-        id: valorComentario,
-        login: "meuUsuario",
-        texto: valorComentario
-      }
-    ];
+    // const novaLista = "";
+    // [
+    //   ...foto.comentarios,
+    //   {
+    //     id: valorComentario,
+    //     login: "meuUsuario",
+    //     texto: valorComentario
+    //   }
+    // ];
 
-    const fotoAtualizada = {
-      ...foto,
-      comentarios: novaLista
-    };
+    // const fotos = this.state.fotos.map(foto =>
+    //   foto.id === fotoAtualizada.id ? fotoAtualizada : foto
+    // );
 
-    const fotos = this.state.fotos.map(foto =>
-      foto.id === fotoAtualizada.id ? fotoAtualizada : foto
-    );
-
-    this.setState({
-      fotos
-    });
-    inputComentario.clear();
+    // this.setState({
+    //   fotos
+    // });
   }
   render() {
     return (
